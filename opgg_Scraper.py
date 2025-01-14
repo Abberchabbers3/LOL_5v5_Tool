d6fea7ce5f8d51094a1960cafb00dc7fa2f12623
justin <jmanlewman@gmail.com>
2025-01-14 14:35:34 -0800

    Added player role scraping, fixed bug with player role scraping

--- opgg_Scraper.py
+++ opgg_Scraper.py
@@ -1,3 +1,5 @@
+import time
+
 import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -5,6 +7,7 @@ from selenium.webdriver.common.by import By
 from collections import defaultdict
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.color import Color
 
 
 class OpggScraper:
@@ -17,10 +20,12 @@ class OpggScraper:
         self.link_map = dict()
         self.player_ranks = defaultdict(dict)
         self.player_game_ranks = defaultdict(list)
+        self.player_recent_roles = defaultdict(lambda: dict(top=0, jungle=0, mid=0, adc=0, supp=0))
         self.driver_options = Options()
         self.driver_options.add_argument('--blink-settings=imagesEnabled=false')
         self.driver_options.add_argument('--disable-javascript')
         self.driver = webdriver.Chrome(options=self.driver_options)
+        self.prev_button = 'TOTAL'
 
         if player_name:
             self.add_player_by_name(server, player_name)
@@ -58,6 +63,7 @@ class OpggScraper:
             output += player + "\n"
             output += str(self.player_ranks[player]) + "\n"
             output += str(self.player_game_ranks[player]) + "\n"
+            output += str(self.player_recent_roles[player]) + "\n"
         return output
 
     def scrape(self, player, link):
@@ -70,16 +76,18 @@ class OpggScraper:
         self.update_profile(self.driver)
 
         # Scrape players current ranks
-        player_ranks = self.get_current_player_ranks(self.driver)
+        self.player_ranks[player] = self.get_current_player_ranks(self.driver)
 
         # Scrape players past ranks
-        player_ranks.update(self.get_past_player_ranks(self.driver))
+        self.player_ranks[player].update(self.get_past_player_ranks(self.driver))
 
-        # Get avg elo of last 20 games
-        game_ranks = self.recent_elo(self.driver)
+        # Get avg elo of last 20 norm games
+        self.player_game_ranks[player] = self.recent_elo(self.driver, "NORMAL", player)
+        recent_game_roles = self.update_recent_roles(self.driver, "NORMAL",1, player)
 
-        self.player_ranks[player] = player_ranks
-        self.player_game_ranks[player] = game_ranks
+        #Get avg elo of last 20 ranked games
+        self.player_game_ranks[player].extend(self.recent_elo(self.driver, "SOLORANKED", player))
+        recent_game_roles.extend(self.update_recent_roles(self.driver, "SOLORANKED",2,  player))
 
     def update_profile(self, driver):
         update_button = driver.find_element(By.XPATH, '//button//span[text()="Update"]')
@@ -117,24 +125,85 @@ class OpggScraper:
         except selenium.common.exceptions.NoSuchElementException:
             return dict()
 
-    def recent_elo(self, driver):
-        # TODO click normal and/or ranked and get those games
+    def change_game_mode(self, driver, game_mode, player):
+        game_mode = game_mode.upper()
+        start_url = driver.current_url
+        if game_mode in start_url:
+            return True
+        # First look for the button
+        try:
+            rank_button = driver.find_element(by=By.XPATH, value=f"//button[@value='{game_mode}']")
+            rank_button.click()
+        # if not there, click the "Queue Type button and try again"
+        except selenium.common.exceptions.NoSuchElementException:
+            try:
+                rank_button = driver.find_element(by=By.XPATH, value=f"//select[@id='queueType']/option[@value='{game_mode}']")
+                rank_button.click()
+            except selenium.common.exceptions.NoSuchElementException:
+                return False
+        # Wait until new page has loaded
+        WebDriverWait(driver, 5).until(
+            EC.url_contains(game_mode)
+        )
+        #TODO Its possible for literally nothing to change during this time but the selected button; however this does not mean the rest has loaded
+        time.sleep(1)
+        return True
+
+    # def change_game_mode(self, driver, game_mode, player_name):
+    #     start_url = driver.current_url
+    #     if game_mode in start_url:
+    #         return True
+    #     try:
+    #         link = self.link_map[player_name]
+    #         driver.get(link+f"?queue_type={game_mode}")
+    #         WebDriverWait(self.driver, 5).until(
+    #             EC.presence_of_element_located((By.XPATH, "//button//span[text()='Update']"))
+    #         )
+    #     except selenium.common.exceptions:
+    #         return False
+    #     return True
+
+    def update_recent_roles(self, driver, game_mode, mode_weight, curr_player_name):
+        if not self.change_game_mode(driver,game_mode, curr_player_name):
+            return []
+        try:
+            all_players = driver.find_elements(by=By.XPATH, value="//div[@class='summoner-tooltip']//span")
+            player_list = [p.get_attribute('textContent') for p in all_players]
+            mod_player_name = curr_player_name.split("#")[0]
+            player_locs = [((i+1)%10)%5 for i,p in enumerate(player_list) if p==mod_player_name]
+            # print(f"num {game_mode} games = {len(player_locs)}, total_players = {len(player_list)}")
+            roles = ["supp", "top", "jungle", "mid", "adc"]
+            recent_games = []
+            for game in player_locs:
+                recent_games.append(game)
+                self.player_recent_roles[curr_player_name][roles[game]] += mode_weight
+            return recent_games
+        except selenium.common.exceptions.NoSuchElementException:
+            return []
+        except selenium.common.exceptions.StaleElementReferenceException:
+            return self.update_recent_roles(driver,game_mode,mode_weight,curr_player_name)
+
+    def recent_elo(self, driver, game_mode, player):
+        if not self.change_game_mode(driver, game_mode, player):
+            return []
         try:
             elo_list = driver.find_elements(by=By.CLASS_NAME, value="avg-tier")
             return [e.text for e in elo_list]
         except selenium.common.exceptions.NoSuchElementException:
             return []
+        except selenium.common.exceptions.StaleElementReferenceException:
+            return self.recent_elo(driver, game_mode, player)
 
     def quit_driver(self):
         self.driver.quit()
 
 
 if __name__ == '__main__':
-    # scraper = OpggScraper("na", "ArCaNeAscension#THICC")  # test for unranked
-    # scraper = OpggScraper("na", "soren#wolf", auto_scrape=True)  # test for both ranked
+    # scraper = OpggScraper("na", "ArCaNeAscension#THICC", auto_scrape=True)  # test for unranked
+    scraper = OpggScraper("na", "soren#wolf", auto_scrape=True)  # test for both ranked
     # scraper = OpggScraper("na", "Oreozx#NA1")  # test for just solo ranked
     # scraper = OpggScraper(link="https://www.op.gg/summoners/na/SpaceDust-balls", auto_scrape=True)
-    scraper = OpggScraper(link="https://www.op.gg/summoners/na/Nation%20Of%20Nugs-NA1", auto_scrape=True)
+    # scraper = OpggScraper(link="https://www.op.gg/summoners/na/Nation%20Of%20Nugs-NA1", auto_scrape=True)
     # p_list = [("na", "ArCaNeAscension#THICC"), ("na", "soren#wolf"), ("na", "Oreozx#NA1"),
     #           "https://www.op.gg/summoners/na/SpaceDust-balls", "https://www.op.gg/summoners/na/Nation%20Of%20Nugs-NA1"]
     # p_list.extend(["https://www.op.gg/summoners/na/Clítorís-42069", "https://www.op.gg/summoners/na/Kelso-69420",
